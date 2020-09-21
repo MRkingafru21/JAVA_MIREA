e02f79f9c275054c2de4109d26a25097fbeaf532
MRkingafru21 <63705392+MRkingafru21@users.noreply.github.com>
2020-09-21 07:37:37 +0300

    ПРАКТИЧЕСКАЯ РАБОТА №1

---
+++ company/TestBall2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TestBall2 {
+    public static void main(String[] args) {
+        Ball2 b1 = new Ball2(150, 300);
+        System.out.println(b1);
+        b1.move(45, 25);
+        System.out.println(b1);
+    }
+}
